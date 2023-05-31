32bcb81cbb2ba88b418a1d243848f06c6d2e656f
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-05-31 13:35:34 -0400

    Create api_instance.py

---
+++ app/api/api_instance.py
@@ -0,0 +1,7 @@
+from flask_restx import Api
+
+api = Api(
+    version='1.0',
+    title='Text PreProcessor API',
+    description='This API provides a comprehensive, easy-to-use toolkit for processing text data.'
+)
