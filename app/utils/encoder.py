10e63ec2c024073c90e5f939f911d9966644cd51
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-05-31 15:52:48 -0400

    Create encoder.py

---
+++ app/utils/encoder.py
@@ -0,0 +1,25 @@
+def encode_text(text: str, encoding: str = 'utf-8', errors: str = 'strict') -> bytes:
+    """
+    This method encodes given text using a specified encoding.
+
+    Parameters:
+    - text (str): The input text to encode.
+    - encoding (str): The encoding type to use. Defaults to 'utf-8'.
+    - errors (str): The error handling strategy to use. Defaults to 'strict'.
+
+    Returns:
+    - bytes: The encoded text.
+
+    Raises:
+    ValueError: If an unsupported encoding type or error handling strategy is specified.
+    """
+    encodings = ['utf-8', 'ascii']
+    if encoding not in encodings:
+        raise ValueError(
+            "Invalid encoding type. Only 'utf-8' and 'ascii' are supported.")
+
+    if errors not in ['strict', 'ignore', 'replace']:
+        raise ValueError(
+            "Invalid error handling strategy. Only 'strict', 'ignore', and 'replace' are supported.")
+
+    return text.encode(encoding, errors=errors)
