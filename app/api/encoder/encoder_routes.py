519ad2618a75c988c77690396d4583def45fcff9
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-06-01 12:46:37 -0400

    Create encoder_routes.py

---
+++ app/api/encoder/encoder_routes.py
@@ -0,0 +1,36 @@
+# Import third-party libraries
+import inspect
+from flask_restx import Resource
+from typing import Dict, Any
+
+# Import project code
+from . import encoder_utils
+from .encoder_models import *
+from ..api_instance import api
+from log_config import Logger
+
+logger = Logger().get_logger()
+
+@encoder_ns.route("/encode_text")
+class EncodeTextResource(Resource):
+    @encoder_ns.doc(description=inspect.getdoc(encoder_utils.encode_text))
+    @encoder_ns.expect(encode_text_model)
+    def post(self):
+        """
+        Encodes the input text using a specified encoding.
+        """
+        try:
+            data: Dict[str, Any] = api.payload
+            text: str = data.get("text", "")
+            encoding: str = data.get("encoding", "utf-8")
+            errors: str = data.get("errors", "strict")
+
+            if not text:
+                return {"error": "No text provided."}, 400
+            
+            result = encoder_utils.encode_text(text, encoding, errors)
+            return {"result": result}, 200
+        
+        except Exception as e:
+            logger.exception("An error occurred during the encoding process.")
+            return {"error": f"An unexpected error occurred: {str(e)}"}, 500
