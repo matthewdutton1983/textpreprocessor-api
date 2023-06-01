a2b9a925cea33c5acda974b59f4315cc23eaeb0f
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-06-01 12:47:52 -0400

    Delete app directory

--- app/api/api_instance.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import Api
-
-api = Api(
-    version="1.0",
-    title="Text PreProcessing API",
-    description="This API provides a collection of text processing utilities that can be used to encode, flatten, normalize, segment, and transform text. The API is organized into different namespaces, each containing related functions."
-)
